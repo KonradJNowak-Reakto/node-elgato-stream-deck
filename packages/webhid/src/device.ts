5a0e8b9cec298bf168d05993e487b5681711c6d8
KonradJNowak-Reakto <128791633+KonradJNowak-Reakto@users.noreply.github.com>
2023-05-22 14:29:06 +0200

    to ee3

--- packages/webhid/src/device.ts
+++ packages/webhid/src/device.ts
@@ -1,5 +1,5 @@
 import { HIDDevice as CoreHIDDevice } from '@elgato-stream-deck/core'
-import { EventEmitter } from 'events'
+import { EventEmitter } from 'eventemitter3'
 import Queue from 'p-queue'
 
 /**
